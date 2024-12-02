0ec739538f46bd7ecbaa5e49f7571ab5ae7a2d2d
zwane71 <makhohlisatemba@gmail.com>
2024-12-02 16:44:26 +0200

    done creating choose mode page

--- spotify/lib/presentation/intro/pages/get_started.dart
+++ spotify/lib/presentation/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStatrtedPage extends StatelessWidget {
   const GetStatrtedPage({super.key});
@@ -24,7 +25,11 @@ class GetStatrtedPage extends StatelessWidget {
                 AppImages.introBG
               ))
             ),
-            child: Column(
+           ),
+           Container(
+            color: Colors.black.withOpacity(0.15),
+           ),
+            Column(
               children: [
                 Align(
                   alignment: Alignment.topCenter,
@@ -51,15 +56,12 @@ class GetStatrtedPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 20,),
                 BasicAppButton(onPressed: (){
-                  
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const ChooseModePage()));
                 }, title: 'Get Started')
               ],
             ),
-           ),
-           Container(
-            color: Colors.black.withOpacity(0.15),
-           )
         ],
+        
       ),
     );
   }
