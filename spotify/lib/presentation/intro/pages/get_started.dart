6354d7510ded394395f3f2cf8e1855a87fe4436c
zwane71 <makhohlisatemba@gmail.com>
2024-12-02 16:02:23 +0200

    implementing get startedpage finished

---
+++ spotify/lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class GetStatrtedPage extends StatelessWidget {
+  const GetStatrtedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+           Container(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal:  40),
+            
+            decoration: const BoxDecoration(
+              image:  DecorationImage(
+                fit: BoxFit.fill,
+                image: 
+              AssetImage(
+                AppImages.introBG
+              ))
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,color: Colors.white,
+                    fontSize: 18
+                  ),
+                ),
+                const SizedBox(height: 21,),
+                const Text(
+                  'Let your body cool and move to the sound of the beat, feel the rhythm take over, as the music sets you free. Surrender to the melody, let your soul dance with every pulse of the sound.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,color: AppColors.grey,
+                    fontSize: 13 
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20,),
+                BasicAppButton(onPressed: (){
+                  
+                }, title: 'Get Started')
+              ],
+            ),
+           ),
+           Container(
+            color: Colors.black.withOpacity(0.15),
+           )
+        ],
+      ),
+    );
+  }
+}
