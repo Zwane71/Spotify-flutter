9ef5b3510aa7973c272a605f001be81091ea1f0d
zwane71 <makhohlisatemba@gmail.com>
2024-12-02 14:55:37 +0200

    defining fonts

--- spotify/lib/main.dart
+++ spotify/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
       home: Container()
     );
   }
