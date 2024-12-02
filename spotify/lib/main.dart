57979559a64f9098bce52639575c901f725d1062
zwane71 <makhohlisatemba@gmail.com>
2024-12-02 15:28:03 +0200

    working on the splash page done

--- spotify/lib/main.dart
+++ spotify/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:  false,
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
-      home: Container()
+      home: const SplashPage()
     );
   }
 }
