e44665bb610ff4c77e7f67adb65e29b85e98eb43
zwane71 <makhohlisatemba@gmail.com>
2024-12-04 10:35:06 +0200

    created theme logic complete

--- spotify/lib/presentation/choose_mode/bloc/theme_cubit.dart
+++ spotify/lib/presentation/choose_mode/bloc/theme_cubit.dart
@@ -1,27 +1,29 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class ThemeCubit extends HydratedCubit<ThemeMode> {
-
   ThemeCubit() : super(ThemeMode.system);
 
   void updateTheme(ThemeMode themeMode) => emit(themeMode);
 
-  
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+    // Convert the string stored in JSON to a ThemeMode enum.
+    final themeString = json['themeMode'] as String;
+    switch (themeString) {
+      case 'dark':
+        return ThemeMode.dark;
+      case 'light':
+        return ThemeMode.light;
+      case 'system':
+      default:
+        return ThemeMode.system;
+    }
   }
 
   @override
   Map<String, dynamic>? toJson(ThemeMode state) {
-    // TODO: implement toJson
-    throw UnimplementedError();
+    // Serialize the ThemeMode enum to a string.
+    return {'themeMode': state.toString().split('.').last}; // Extract just 'dark', 'light', or 'system'
   }
-
-
-
 }
